301d03cd1ab43bdc6532a52273790b5a79603695
Andrés.ds equipo1 <aonikenk.ds@gmail.com>
2026-01-05 15:49:22 -0300

    Fix error static files Jazzmin

--- portal_empleos/settings.py
+++ portal_empleos/settings.py
@@ -147,7 +147,7 @@ SERVER_EMAIL = "noreply@buscapegachile.cl"
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
